3df6e87c05de6ce078b1d4b77711ee0b04762a30
Damien Bonnet <bonnet.damien@ymail.com>
2022-06-07 00:07:52 +0200

    [:sparkles: FEAT]  hero banner

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,12 @@
+import { Layout } from '@/components/layout';
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Layout>
+      <div className='home__hero'>
+        <img src='/images/bg-hero-home.jpg' alt='Hero home' />
+        <span>Chez vous, partout et ailleurs</span>
+      </div>
+    </Layout>
+  );
 };
