350113144178d5b799346f4ca32c4d347f22d9bc
Damien Bonnet <bonnet.damien@ymail.com>
2022-06-07 16:27:04 +0200

    [:sparkles: FEAT]  error page

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,3 +1,16 @@
+import { Layout } from '@/components/layout';
+import { Link } from 'react-router-dom';
+
 export const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Layout hideFooter flex>
+      <div className='error'>
+        <div className='error__container'>
+          <h1>404</h1>
+          <p>Oups! La page que vous demandez n'existe pas.</p>
+          <Link to='/'>Retourner sur la page d’accueil</Link>
+        </div>
+      </div>
+    </Layout>
+  );
 };
