b67d4affdcd9b891296aaa593a10cfe7e3ee9e82
Damien Bonnet <bonnet.damien@ymail.com>
2022-06-07 13:46:21 +0200

    [:art: FORMAT]  import house template

--- src/pages/houseId.tsx
+++ src/pages/houseId.tsx
@@ -1,3 +1,29 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { HouseIdTemplate } from '@/components/template';
+
+import { housesService } from '@/api/houses.service';
+import { House } from '@/ts';
+
 export const HouseId = () => {
-  return <div>HouseId</div>;
+  const [house, setHouse] = useState<House | undefined>(undefined);
+  const { houseId } = useParams();
+  const navigate = useNavigate();
+
+  const fetchHouse = useCallback(async () => {
+    const data = await housesService.getById(houseId || '');
+    if (!data) {
+      return navigate('/not-found');
+    }
+    setHouse(data);
+  }, [houseId, navigate]);
+
+  useEffect(() => {
+    fetchHouse();
+  }, [fetchHouse]);
+
+  if (!house) return null;
+
+  return <HouseIdTemplate house={house} />;
 };
